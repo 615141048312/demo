acf27c761449b70a3ef42b390b9472cce44323c1
615141048312 <hanada.yui@mbr.nifty.com>
2020-10-31 18:31:33 +0900

    create registration

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.data.entity.User;
 import com.example.demo.data.repsitory.UserRepository;
+import com.example.demo.form.UserForm;
 
 @Controller
 public class UserController {
@@ -20,5 +22,21 @@ public class UserController {
 		List<User> users = userRepository.findAll();
 		model.addAttribute("users", users);
 		return "users";
+	}
+	
+	@GetMapping("/newuser")
+	public String getNewUser(Model model) {
+		UserForm userForm = new UserForm();
+		model.addAttribute("userForm", userForm);
+		return "newuser";
+	}
+	
+	@PostMapping("/newuser")
+	public String registerUser(UserForm userForm) {
+		User user = new User();
+		user.setName(userForm.getName());
+		user.setEmail(userForm.getEmail());
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
