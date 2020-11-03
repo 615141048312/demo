b6623456c42b28f47914304e785d6dc54ed489cb
615141048312 <hanada.yui@mbr.nifty.com>
2020-11-03 12:52:45 +0900

    finish 11

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.data.entity.User;
@@ -32,11 +36,20 @@ public class UserController {
 	}
 	
 	@PostMapping("/newuser")
-	public String registerUser(UserForm userForm) {
+	public String registerUser(@Validated UserForm userForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "newuser";
+		}
 		User user = new User();
 		user.setName(userForm.getName());
 		user.setEmail(userForm.getEmail());
 		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public String deleteUser(@PathVariable Long id) {
+		userRepository.deleteById(id);
 		return "redirect:/users";
 	}
 }
